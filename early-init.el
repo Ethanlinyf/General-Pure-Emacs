0aa5bac741e9a232eb52e7c7273bcf6377369187
Ethanlinyf <e.yflin@gmail.com>
2022-06-21 12:57:16 +1000

    update tutorial for early-init.el

--- early-init.el
+++ early-init.el
@@ -16,7 +16,7 @@
 (setq debug-on-error t)
 
 ;; Puremacs is compatible from the emacs version 27.1 -TE
-(let ((minver "27.1"))
+(let ((minver "28.1"))
 (when (version< emacs-version minver)
   (error "Puremacs requires V%s or higher versions." minver)))
 
@@ -30,11 +30,27 @@
 ;; After early-init-file to initialise 'package' -TE
 (setq package-enable-at-startup nil)
 
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file. -- Doom Emacs
+(setq load-prefer-newer noninteractive)
+
+;; Inhibit resising Puremacs frame
+(setq frame-inhibit-implied-resize t)
+
+  ;; Premature redisplays can substantially affect startup times and produce
+  ;; ugly flashes of unstyled Emacs.
+  (setq-default inhibit-redisplay t
+                inhibit-message t)
+  (add-hook 'window-setup-hook
+            (lambda ()
+              (setq-default inhibit-redisplay nil
+                            inhibit-message nil)
+              (redisplay)))
+
 ;; Default encoding system -TE
 (set-language-environment "UTF-8")
 
-;; Inhibit resising Puremacs frame
-;; (setq frame-inhibit-implied-resize t)
 
 ;; Remove some warnings
 ;;(setq load-prefer-newer t)
