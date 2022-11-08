769fac42645ff57b4051e1165541bea666aac1f5
Ethanlinyf <e.yflin@gmail.com>
2022-11-09 09:04:24 +1000

    add toggle-one-window and disable dashboard

--- site-lisp/pure-function.el
+++ site-lisp/pure-function.el
@@ -96,5 +96,22 @@ Similar to `marginalia-annotate-symbol', but does not show symbol class."
     (deactivate-mark)))
 
 ;;--------------------------------------------------------------------
+;; toggle-one-window, from Andy Stewart
+(defvar toggle-one-window-window-configuration nil
+  "The window configuration use for `toggle-one-window'.")
+
+(defun toggle-one-window ()
+  "Toggle between window layout and one window."
+  (interactive)
+  (if (equal (length (cl-remove-if #'window-dedicated-p (window-list))) 1)
+      (if toggle-one-window-window-configuration
+          (progn
+            (set-window-configuration toggle-one-window-window-configuration)
+            (setq toggle-one-window-window-configuration nil))
+        (message "No other windows exist."))
+    (setq toggle-one-window-window-configuration (current-window-configuration))
+    (delete-other-windows)))
+
+;;--------------------------------------------------------------------
 (provide 'pure-function)
 ;;; pure-function.el ends here
