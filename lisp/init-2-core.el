7271cb6c30ff6c9f4f492beada2be8caeb4649dd
Ethanlinyf <e.yflin@gmail.com>
2022-11-19 12:48:50 +1000

    update README with the framework first

---
+++ lisp/init-2-core.el
@@ -0,0 +1,16 @@
+;;; init-core.el --- Foundamental settings for Emacs. -*- lexical-binding: t; -*-
+;;
+;; Copyleft (CL) 2022-2032 YF Lin
+;;
+;; Something good as indicated, by Dr YF Lin <e.yflin@gmail.com>
+;; URL: https://github.com/Ethanlinyf/General-Pure-Emacs
+;; Under ThingsEngine Project: https://www.thethingsengine.org
+;;--------------------------------------------------------------------
+;;; Commentary:
+;; Fundamental settings for General Pure Emacs
+;;--------------------------------------------------------------------
+;;; Code:
+
+;;--------------------------------------------------------------------
+(provide 'init-2-core)
+;;; init-core.el ends here.
