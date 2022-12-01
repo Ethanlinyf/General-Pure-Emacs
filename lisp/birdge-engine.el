dbba31a2b73de0a54741319f39ada742b1e2c8d7
Ethanlinyf <e.yflin@gmail.com>
2022-12-01 20:49:42 +1000

    update bridge-engine.el

--- lisp/birdge-engine.el
+++ lisp/birdge-engine.el
@@ -7,7 +7,7 @@
 ;; Under ThingsEngine Project: https://www.thethingsengine.org
 ;;--------------------------------------------------------------------
 ;;; Commentary:
-;; Add needed plugins to build General Pure Emacs
+;; Add needed plugins to build General Pure Emacs if you are not using use-package
 ;;--------------------------------------------------------------------
 ;;; Code:
 
@@ -24,126 +24,7 @@
   (exec-path-from-shell-initialize))
 ;; Add whatever packages you want here
 (defvar puremacs/packages '(
-			    company
-                            ;; company-box
-			    ;; smartparens
-                            all-the-icons-completion
-
-                            undo-tree
-
-                            ;; ;; lsp-mode
-                            ;; flycheck
-                            ;; 
-                            treemacs
-                            ;; highlight-indent-guides
-                            multi-term
-                            ;; ido
-                            ;; ivy-rich
-                            ;; all-the-icons-ivy-rich
-                            ;; amx
-                            all-the-icons-dired
-                            diredfl
-                            ;;;diredful
-                            ;; maxframe
-                            ns-auto-titlebar ;; Set the MacOS transparent titlebar to match theme
-                            ;; 
-                            ;; 
-                            ;; 
-                            ;; pangu-spacing ; Minor-mode to add space between Chinese and English characters.
-                            all-the-icons-ibuffer ; Display icons for all buffers in ibuffer.
-                            ;; all-the-icons-ivy-rich ;
-                            ;; good-scroll ; This package implements smooth scrolling by pixel lines. It attempts to improve upon `pixel-scroll-mode' by adding variable speed.
-                            ;; elisp-format
-                            ;; 
-                            ;; 
-                            ;; org-download
-                            ;; format-all
-                            ;; htmlize ; from org-site
-                            ;; ;;ox-twbs
-                            
-                            ;; 
-                            treemacs-all-the-icons
-                            corfu
-                            orderless
-                            ;; 
-                            ;; eglot
-                            epc
-                            corfu-doc
-                            lin
-
-                            ;;; D
-                            which-key
-                            window-numbering
-                            magit
-                            rainbow-delimiters
-                            hungry-delete
-                            ctrlf
-                            popwin
-                            minions
-                            cape
-                            ;; format-all
-                            ;; crux
-
-                            
-                            ;;; G
-                            yasnippet
-
-                            ;;; H
-                            posframe
-                            markdown-mode
-
-                            ;;; I
-                            pyvenv
-
-                            ;;org
-                            org-superstar
-                            org-noter
-                            org-ql
-                            org-download
-                            pangu-spacing
-                            ;;valign installed
-
-                            ;;org-roam
-                            org-roam
-                            org-roam-ui
-                            org-roam-bibtex
-                            websocket
-
-                            dash
-                            f
-                            s
-                            emacsql
-                            emacsql-sqlite
-                            magit-section
-                            ;; [filenotify-recursive][https://github.com/jethrokuan/filenotify-recursive]
-
-                            ;; programming
-                            
-                            julia-repl
-
-
-                            ;; TeX
-                            auctex
-                            reftex
-                            cdlatex
-                            company-auctex
-                            pdf-tools
-                            latex-preview-pane ;;latex-preview-pane is a minor mode for Emacs that enables you to preview your LaTeX files directly in Emacs. It supports PDF previews, your choice of pdflatex or xelatex, and it highlights errors in your LaTeX buffer.
-                            zotxt ; for reference
-
-                            ;;; Website
-                            htmlize
-
-                            ;;; Optional
-                            keycast
-                            ef-themes
-
-                            ;;; Programming
-                            lua-mode
-
-                            ;;; deno-bridge:
-                            websocket ;; dependency for the package, org-roam-ui-20220225.2151
-                            
+			    org-superstar
                                  )  "Default packages.")
 
 (setq package-selected-packages puremacs/packages)
