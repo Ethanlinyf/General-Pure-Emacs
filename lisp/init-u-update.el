f8a634ac106444c5a01c5507c284bb1bcdbdbad3
Ethanlinyf <e.yflin@gmail.com>
2023-02-21 13:53:04 +1000

    add init-u-update.el for version or packages update.

---
+++ lisp/init-u-update.el
@@ -0,0 +1,30 @@
+;;; init-update.el --- Foundamental settings for Emacs. -*- lexical-binding: t; -*-
+;;
+;; Copyleft (CL) 2022-2032 YF Lin
+;;
+;; Something good as indicated, by Dr YF Lin <e.yflin@gmail.com>
+;; URL: https://github.com/Ethanlinyf/General-Pure-Emacs
+;; Under ThingsEngine Project: https://www.thethingsengine.org
+;;--------------------------------------------------------------------
+;;; Commentary:
+;; This will be used for the Emacs update from V29.1 for GPE
+;;--------------------------------------------------------------------
+;;; Code:
+
+;; Settings for the update of Emacs 29.1
+
+;;------------------ Emacs@29 ---------------------------------------
+;; (when emacs/>=29p
+;;   (push '(sh-mode . bash-ts-mode) major-mode-remap-alist)
+;;   (push '(c-mode . c-ts-mode) major-mode-remap-alist)
+;;   (push '(c++-mode . c++-ts-mode) major-mode-remap-alist)
+;;   (push '(css-mode . css-ts-mode) major-mode-remap-alist)
+;;   (push '(javascript-mode . js-ts-mode) major-mode-remap-alist)
+;;   (push '(js-json-mode . json-ts-mode) major-mode-remap-alist)
+;;   (push '(python-mode . python-ts-mode) major-mode-remap-alist))
+
+;;--------------------------------------------------------------------
+
+;;--------------------------------------------------------------------
+(provide 'init-u-update)
+;;; init-u-update.el ends here
