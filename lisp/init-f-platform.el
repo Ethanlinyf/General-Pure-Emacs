5e00acf3b768b4eb01be690569edca9c04d552d7
Ethanlinyf <e.yflin@gmail.com>
2023-02-28 22:53:49 +1000

    add centaur-tabs with configuration

--- lisp/init-f-platform.el
+++ lisp/init-f-platform.el
@@ -18,21 +18,41 @@
   :init
   (setq magit-diff-refine-hunk t))
 ;;------------------------------------------------------------------
-
-;; add tabs for the open files
-(use-package awesome-tab
-  :ensure nil
-  :load-path "~/.emacs.d/site-lisp/awesome-tab"
-  :hook
-  (after-init . awesome-tab-mode)
+(use-package centaur-tabs
+  :init
+  (setq centaur-tabs-enable-key-bindings t)
+  ;; (centaur-tabs-mode t)
   :config
-  (defun awesome-tab-buffer-groups ()
-    "`awesome-tab-buffer-groups' control buffers' group rules.
-Group awesome-tab with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+  (setq centaur-tabs-style "bar"
+        ;; centaur-tabs-height 32
+        ;; centaur-tabs-set-icons t
+        centaur-tabs-show-new-tab-button t
+        centaur-tabs-set-modified-marker t
+        centaur-tabs-show-navigation-buttons t
+        centaur-tabs-set-bar 'left
+        centaur-tabs-show-count nil
+        ;; centaur-tabs-label-fixed-length 15
+        ;; centaur-tabs-gray-out-icons 'buffer
+        ;; centaur-tabs-plain-icons t
+        x-underline-at-descent-line t
+        centaur-tabs-left-edge-margin nil)
+  ;; (centaur-tabs-change-fonts (face-attribute 'default :font) 110)
+  ;; (centaur-tabs-headline-match)
+  ;; (centaur-tabs-enable-buffer-alphabetical-reordering)
+  ;; (setq centaur-tabs-adjust-buffer-order t)
+  
+  (setq uniquify-separator "/")
+  (setq uniquify-buffer-name-style 'forward)
+  (defun centaur-tabs-buffer-groups ()
+    "`centaur-tabs-buffer-groups' control buffers' group rules.
+
+Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
 All buffer name start with * will group to \"Emacs\".
-Other buffer group by `awesome-tab-get-group-name' with project name."
+Other buffer group by `centaur-tabs-get-group-name' with project name."
     (list
      (cond
+      ;; ((not (eq (file-remote-p (buffer-file-name)) nil))
+      ;; "Remote")
       ((or (string-equal "*" (substring (buffer-name) 0 1))
            (memq major-mode '(magit-process-mode
                               magit-status-mode
@@ -43,39 +63,76 @@ Other buffer group by `awesome-tab-get-group-name' with project name."
                               magit-blame-mode
                               )))
        "Emacs")
-      ((derived-mode-p 'eshell-mode)
-       "EShell")
-      ((derived-mode-p 'emacs-lisp-mode)
-       "Elisp")
+      ((derived-mode-p 'prog-mode)
+       "Editing")
       ((derived-mode-p 'dired-mode)
        "Dired")
-      ((memq major-mode '(org-mode org-agenda-mode diary-mode))
+      ((memq major-mode '(helpful-mode
+                          help-mode))
+       "Help")
+      ((memq major-mode '(org-mode
+                          org-agenda-clockreport-mode
+                          org-src-mode
+                          org-agenda-mode
+                          org-beamer-mode
+                          org-indent-mode
+                          org-bullets-mode
+                          org-cdlatex-mode
+                          org-agenda-log-mode
+                          diary-mode))
        "OrgMode")
       (t
-       (awesome-tab-get-group-name (current-buffer))))))
+       (centaur-tabs-get-group-name (current-buffer))))))
 
-  (defun awesome-tab-hide-tab (x)
+  (defun centaur-tabs-hide-tab (x)
+    "Do no to show buffer X in tabs."
     (let ((name (format "%s" x)))
       (or
+       ;; Current window is not dedicated window.
+       (window-dedicated-p (selected-window))
+
+       ;; Buffer name not match below blacklist.
        (string-prefix-p "*epc" name)
        (string-prefix-p "*helm" name)
+       (string-prefix-p "*Helm" name)
        (string-prefix-p "*Compile-Log*" name)
        (string-prefix-p "*lsp" name)
-       (string-prefix-p "Aweshell" name)
-       (string-prefix-p "*shell*" name)
-       (string-prefix-p "*shell" name)
+       (string-prefix-p "*company" name)
+       (string-prefix-p "*Flycheck" name)
+       ;; (string-prefix-p "Aweshell" name)
+       (string-prefix-p "*tramp" name)
+       (string-prefix-p " *Mini" name)
+       (string-prefix-p "*help" name)
+       (string-prefix-p "*straight" name)
+       (string-prefix-p " *temp" name)
+       (string-prefix-p "*Help" name)
+       (string-prefix-p "*mybuf" name)
        (string-prefix-p "*dashboard*" name)
-       (string-prefix-p "*info*" name)
        (string-prefix-p "*scratch*" name)
        (string-prefix-p "*Messages*" name)
+       (string-prefix-p "*Shell Command Output*" name)
+       
+
+       ;; Is not magit buffer.
        (and (string-prefix-p "magit" name)
             (not (file-name-extension name)))
        )))
+  
+  :hook
+  ;; (after-init . centaur-tabs-mode)
+  (term-mode . centaur-tabs-local-mode)
+  (calendar-mode . centaur-tabs-local-mode)
+  (org-agenda-mode . centaur-tabs-local-mode)
+  (aweshell-dedicated-open . centaur-tabs-local-mode)
+  (after-init . centaur-tabs-mode)
+  ;; :bind
+  ;; ("C-<prior>" . centaur-tabs-backward)
+  ;; ("C-<next>" . centaur-tabs-forward)
+  ;; ("C-S-<prior>" . centaur-tabs-move-current-tab-to-left)
+  ;; ("C-S-<next>" . centaur-tabs-move-current-tab-to-right)
+  )
 
-  (when (display-graphic-p)
-    (setq awesome-tab-display-icon t)))
 ;;--------------------------------------------------------------------
-
 ;; treemacs
 (use-package treemacs
   :ensure t
@@ -156,7 +213,6 @@ Other buffer group by `awesome-tab-get-group-name' with project name."
               ("C-x t 1"   . treemacs-delete-other-windows)
               ("C-x t t"   . treemacs)
               ("s-2"       . treemacs)
-                                        ;("s-3"       . awesome-tab-mode)
               ("C-x t d"   . treemacs-select-directory)
               ("C-x t B"   . treemacs-bookmark)
               ("C-x t C-t" . treemacs-find-file)
@@ -266,7 +322,7 @@ Other buffer group by `awesome-tab-get-group-name' with project name."
   ;; 
   (("s-4" . popweb-dict-youdao-pointer)
    ("s-5" . popweb-url-preview-pointer)))
-  ;; :pin manual)
+;; :pin manual)
 ;;--------------------------------------------------------------------
 
 ;; Markmacro
