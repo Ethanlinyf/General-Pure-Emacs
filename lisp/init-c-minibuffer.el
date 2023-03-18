15ee5e2a0462f91ca774facd3747fa55b4d5bbb0
Ethanlinyf <e.yflin@gmail.com>
2023-03-19 08:51:58 +1000

    fix consult botched by preview-key change

--- lisp/init-c-minibuffer.el
+++ lisp/init-c-minibuffer.el
@@ -384,7 +384,7 @@
    consult-bookmark consult-recent-file consult-xref
    consult--source-bookmark consult--source-recent-file
    consult--source-project-recent-file
-   :preview-key (kbd "M-."))
+   :preview-key "M-.")
 
   ;; Optionally configure the narrowing key.
   ;; Both < and C-+ work reasonably well.
