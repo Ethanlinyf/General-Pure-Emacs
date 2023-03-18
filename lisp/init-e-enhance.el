1c0b2db57e22e5a9f98c38d09095752e44de6237
Ethanlinyf <e.yflin@gmail.com>
2023-03-18 18:53:36 +1000

    delete cape and other comment code

--- lisp/init-e-enhance.el
+++ lisp/init-e-enhance.el
@@ -161,50 +161,6 @@
 ;;    ))
 
 ;;--------------------------------------------------------------------
-;; to be deleted
-;; (setq ispell-program-name "hunspell")
-;; (setq ispell-local-dictionary "de_DE")
-;; (setq ispell-local-dictionary-alist
-;;       '(("de_DE" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil nil nil utf-8)))
-
-;;--------------------------------------------------------------------
-;; Add extensions
-;; (use-package cape
-;;   ;; Bind dedicated completion commands
-;;   ;; Alternative prefix keys: C-c p, M-p, M-+, ...
-;;   :bind (("C-c p p" . completion-at-point) ;; capf
-;;          ("C-c p t" . complete-tag)        ;; etags
-;;          ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
-;;          ("C-c p h" . cape-history)
-;;          ("C-c p f" . cape-file)
-;;          ("C-c p k" . cape-keyword)
-;;          ("C-c p s" . cape-symbol)
-;;          ("C-c p a" . cape-abbrev)
-;;          ("C-c p i" . cape-ispell)
-;;          ("C-c p l" . cape-line)
-;;          ("C-c p w" . cape-dict)
-;;          ("C-c p \\" . cape-tex)
-;;          ("C-c p _" . cape-tex)
-;;          ("C-c p ^" . cape-tex)
-;;          ("C-c p &" . cape-sgml)
-;;          ("C-c p r" . cape-rfc1345))
-;;   :init
-;;   ;; Add `completion-at-point-functions', used by `completion-at-point'.
-;;   (add-to-list 'completion-at-point-functions #'cape-file)
-;;   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-;;   (add-to-list 'completion-at-point-functions #'cape-history)
-;;   (add-to-list 'completion-at-point-functions #'cape-keyword)
-;;   (add-to-list 'completion-at-point-functions #'cape-tex)
-;;   (add-to-list 'completion-at-point-functions #'cape-sgml)
-;;   (add-to-list 'completion-at-point-functions #'cape-rfc1345)
-;;   (add-to-list 'completion-at-point-functions #'cape-abbrev)
-;;   (add-to-list 'completion-at-point-functions #'cape-ispell)
-;;   (add-to-list 'completion-at-point-functions #'cape-dict)
-;;   (add-to-list 'completion-at-point-functions #'cape-symbol)
-;;   (add-to-list 'completion-at-point-functions #'cape-line)
-;;   )
-
-;;--------------------------------------------------------------------
 ;; Flycheck
 (use-package flycheck
   :ensure t
@@ -232,22 +188,6 @@
   ;; If it's nil, Emacs tries to automatically set up the dictionaries.
   (when (boundp 'ispell-hunspell-dictionary-alist)
     (setq ispell-hunspell-dictionary-alist ispell-local-dictionary-alist)))
-
-;; (setq ispell-program-name "hunspell")
-;; ;; you could set `ispell-dictionary` instead but `ispell-local-dictionary' has higher priority
-;; (setq ispell-local-dictionary "en_US")
-;; (setq ispell-local-dictionary-alist '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US,en_US-med") nil utf-8)))
-;; ;; new variable `ispell-hunspell-dictionary-alist' is defined in Emacs
-;; ;; If it's nil, Emacs tries to automatically set up the dictionaries.
-;; (when (boundp 'ispell-hunspell-dictionary-alist)
-;;   (setq ispell-hunspell-dictionary-alist ispell-local-dictionary-alist))
-
-;; (eval-after-load "flyspell"
-;;   '(progn
-;;      (define-key flyspell-mouse-map [down-mouse-3] #'flyspell-correct-word)
-;;      (define-key flyspell-mouse-map [mouse-3] #'undefined)
-;;      (define-key flyspell-mouse-map [down-mouse-2] nil)
-;;      (define-key flyspell-mouse-map [mouse-2] nil)))
 
 ;;--------------------------------------------------------------------
 ;; An intuitive and efficient solution for single-buffer text search
@@ -307,13 +247,11 @@
 ;;            (file-name-extension (buffer-name)) t))))
 
 ;;--------------------------------------------------------------------
-
 ;; goto-line-preview
 (use-package goto-line-preview
   :ensure t)
 
 ;;--------------------------------------------------------------------
-
 ;; golden-ratio
 (use-package golden-ratio
   :ensure t
@@ -334,7 +272,7 @@
           (inhibit-message t))
       (apply undo-tree-save-history args)))
   (advice-add 'undo-tree-save-history :around 'my-undo-tree-save-history)
-  :init (global-undo-tree-mode))
+  :init (global-undo-tree-mode)
 ;; :after hydra
 ;; :bind ("C-x C-h u" . hydra-undo-tree/body)
 ;; :hydra (hydra-undo-tree (:hint nil)
@@ -346,6 +284,7 @@
 ;; ("l"   undo-tree-load-history)
 ;; ("u"   undo-tree-visualize "visualize" :color blue)
 ;; ("q"   nil "quit" :color blue)))
+)
 
 ;;--------------------------------------------------------------------
 ;; Elisp API Demos
@@ -440,6 +379,8 @@
 ;;               (deactivate-mark)
 ;;             (rectangle-mark-mode 1))
 ;;       "reset")))))
+
+
 ;;--------------------------------------------------------------------
 ;; tiny to be added.
 ;;--------------------------------------------------------------------
