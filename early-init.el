83995e8cf956f87b661529fca13d55344e57942f
Ethanlinyf <e.yflin@gmail.com>
2023-03-18 14:41:25 +1000

    update early-init.el

--- early-init.el
+++ early-init.el
@@ -15,24 +15,21 @@
 
 ;; Debugging for the setting update.
 (setq debug-on-error nil)
+(when init-file-debug
+  (profiler-start 'cpu)
+  (add-hook 'window-setup-hook #'profiler-stop 0))
 
 ;; Puremacs is compatible from the emacs version 27.1.
 (let ((minver "27.1"))
   (when (version< emacs-version minver)
     (error "The GPE requires V%s or higher versions" minver)))
 
-(when (version< emacs-version "28.1")
-  (message "Your Emacs is old, and some functionality in this configuration will be disabled. Please upgrade to at least 28.2 if possible."))
-
-(when (version< emacs-version "29.0")
-  (message "This configuration of GPE has been optimised based on the version 29.0. Suggest update to Emacs 29."))
-
 ;; Garbage collection in the startup process
 ;; (setq gc-cons-threshold most-positive-fixnum
 ;;       gc-cons-percentage 0.5)
-;; (setq gc-cons-threshold most-positive-fixnum)
 ;; (add-hook 'after-init-hook #'(lambda () (setq gc-cons-threshold 800000)))
 (setq gc-cons-threshold most-positive-fixnum)
+(setq gc-cons-percentage 0.5)
 
 ;; Prevent unwanted run-time compilation for native-comp users
 (when (>= emacs-major-version 29)
@@ -58,35 +55,15 @@
 ;; Inhibit resizing Puremacs frame
 (setq frame-inhibit-implied-resize t)
 
-;; To suppress flashing at startup
-(setq-default inhibit-redisplay t
-              inhibit-message t)
-(add-hook 'window-setup-hook
-          (lambda ()
-            (setq-default inhibit-redisplay nil
-                          inhibit-message nil)
-            (redisplay)))
-
 ;; Remove some warnings
-(setq byte-compile-warnings '(not cl-functions
-                                  nresolved
-                                  free-vars
-                                  callargs
-                                  redefine
-                                  obsolete
-                                  noruntime
-                                  interactive-only
-                                  ))
+(setq byte-compile-warnings nil)
 
 ;; Default settings for the frame before initialisation
 ;; To prevent the glimpse of un-styled Emacs by disabling the following UI elements early.
-;; (push '(menu-bar-lines . 0) default-frame-alist)
-;; (push '(tool-bar-lines . 0) default-frame-alist)
-;; (push '(vertical-scroll-bars) default-frame-alist)
 (setq use-file-dialog nil)
-(menu-bar-mode -1) ;; disables menubar
-(tool-bar-mode -1) ;; disables toolbar
-(scroll-bar-mode -1) ;; disables scrollbar
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
 (when (featurep 'ns)
   (push '(ns-transparent-titlebar . t) default-frame-alist))
 
@@ -95,6 +72,7 @@
 
 ;; Prevent flash of un-styled modeline at startup
 (setq-default mode-line-format nil)
+(fset 'display-startup-echo-area-message 'ignore)
 
 ;;----------------------------------------------------------------------
 ;;; early-init.el ends here.
