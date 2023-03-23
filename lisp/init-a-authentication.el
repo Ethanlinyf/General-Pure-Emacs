194b1e97225b089603341613224ca64e86642cdc
Ethanlinyf <e.yflin@gmail.com>
2023-03-23 16:58:11 +1000

    add authentication for openai

--- lisp/init-a-authentication.el
+++ lisp/init-a-authentication.el
@@ -11,6 +11,9 @@
 ;;--------------------------------------------------------------------
 ;;; Code:
 
+;;; ChatGPT applications:
+;; Obtain OpenAI API Key, and save the API Key to the ~/.emacs.d/mind-wave/chatgpt_api_key.txt file
+;; (Or set the environment variable OPENAI_API_KEY in init-p-setting.el.).
 
 ;;--------------------------------------------------------------------
 (provide 'init-a-authentication)
