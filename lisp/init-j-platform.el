4df1e98c6d27975c824cbb24747f9356d81b1aa0
Ethanlinyf <e.yflin@gmail.com>
2023-08-01 15:16:59 +1000

    Centaur-tabs to replace awesone-tabs with nerd-icons

--- lisp/init-j-platform.el
+++ lisp/init-j-platform.el
@@ -19,21 +19,43 @@
   (setq magit-diff-refine-hunk t))
 
 ;;--------------------------------------------------------------------
-;; add tabs for the open files
-(use-package awesome-tab
-  :ensure nil
-  :load-path "~/.emacs.d/site-lisp/awesome-tab"
-  ;; :hook
-  ;; (after-init . awesome-tab-mode)
-  :bind ("s-9" . awesome-tab-mode)
+;; Centaur-tabs
+
+(use-package centaur-tabs
+  :init
+  (setq centaur-tabs-enable-key-bindings t)
   :config
-  (defun awesome-tab-buffer-groups ()
-    "`awesome-tab-buffer-groups' control buffers' group rules.
-  Group awesome-tab with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
-  All buffer name start with * will group to \"Emacs\".
-  Other buffer group by `awesome-tab-get-group-name' with project name."
+  (setq centaur-tabs-icon-type 'nerd-icons ;; or 'all-the-icons
+        centaur-tabs-style "bar"
+        centaur-tabs-height 32
+        centaur-tabs-set-icons t
+        centaur-tabs-show-new-tab-button t
+        centaur-tabs-set-modified-marker t
+        centaur-tabs-show-navigation-buttons t
+        centaur-tabs-set-bar 'left
+        centaur-tabs-show-count nil
+        ;; centaur-tabs-label-fixed-length 15
+        ;; centaur-tabs-gray-out-icons 'buffer
+        ;; centaur-tabs-plain-icons t
+        x-underline-at-descent-line t
+        centaur-tabs-left-edge-margin nil)
+  ;; (centaur-tabs-change-fonts (face-attribute 'default :font) 110)
+  (centaur-tabs-headline-match)
+  ;; (centaur-tabs-enable-buffer-alphabetical-reordering)
+  ;; (setq centaur-tabs-adjust-buffer-order t)
+  (centaur-tabs-mode t)
+  (setq uniquify-separator "/")
+  (setq uniquify-buffer-name-style 'forward)
+  (defun centaur-tabs-buffer-groups ()
+    "`centaur-tabs-buffer-groups' control buffers' group rules.
+
+Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+All buffer name start with * will group to \"Emacs\".
+Other buffer group by `centaur-tabs-get-group-name' with project name."
     (list
      (cond
+      ;; ((not (eq (file-remote-p (buffer-file-name)) nil))
+      ;; "Remote")
       ((or (string-equal "*" (substring (buffer-name) 0 1))
            (memq major-mode '(magit-process-mode
                               magit-status-mode
@@ -44,37 +66,98 @@
                               magit-blame-mode
                               )))
        "Emacs")
-      ((derived-mode-p 'eshell-mode)
-       "EShell")
-      ((derived-mode-p 'emacs-lisp-mode)
-       "Elisp")
+      ((derived-mode-p 'prog-mode)
+       "Editing")
       ((derived-mode-p 'dired-mode)
        "Dired")
-      ((memq major-mode '(org-mode org-agenda-mode diary-mode))
+      ((memq major-mode '(helpful-mode
+                          help-mode))
+       "Help")
+      ((memq major-mode '(org-mode
+                          org-agenda-clockreport-mode
+                          org-src-mode
+                          org-agenda-mode
+                          org-beamer-mode
+                          org-indent-mode
+                          org-bullets-mode
+                          org-cdlatex-mode
+                          org-agenda-log-mode
+                          diary-mode))
        "OrgMode")
       (t
-       (awesome-tab-get-group-name (current-buffer))))))
+       (centaur-tabs-get-group-name (current-buffer))))))
+  :hook
+  (dashboard-mode . centaur-tabs-local-mode)
+  (term-mode . centaur-tabs-local-mode)
+  (calendar-mode . centaur-tabs-local-mode)
+  (org-agenda-mode . centaur-tabs-local-mode)
+  (eshell-mode . centaur-tabs-local-mode)
+  :bind
+  ("C-<prior>" . centaur-tabs-backward)
+  ("C-<next>" . centaur-tabs-forward)
+  ("C-S-<prior>" . centaur-tabs-move-current-tab-to-left)
+  ("C-S-<next>" . centaur-tabs-move-current-tab-to-right))
 
-  (defun awesome-tab-hide-tab (x)
-    (let ((name (format "%s" x)))
-      (or
-       (string-prefix-p "*epc" name)
-       (string-prefix-p "*helm" name)
-       (string-prefix-p "*Compile-Log*" name)
-       (string-prefix-p "*lsp" name)
-       (string-prefix-p "*Treemacs-" name)
-       (string-prefix-p "*shell*" name)
-       (string-prefix-p "*shell" name)
-       (string-prefix-p "*dashboard*" name)
-       (string-prefix-p "*info*" name)
-       (string-prefix-p "*scratch*" name)
-       (string-prefix-p "*Messages*" name)
-       (and (string-prefix-p "*magit" name)
-            (not (file-name-extension name)))
-       )))
+;;--------------------------------------------------------------------
+;; add tabs for the open files
+;; (use-package awesome-tab
+;;   :ensure nil
+;;   :load-path "~/.emacs.d/site-lisp/awesome-tab"
+;;   ;; :hook
+;;   ;; (after-init . awesome-tab-mode)
+;;   :bind ("s-9" . awesome-tab-mode)
+;;   :init
+;;   (defun awesome-tab-buffer-groups ()
+;;     "`awesome-tab-buffer-groups' control buffers' group rules.
 
-  (when (display-graphic-p)
-    (setq awesome-tab-display-icon t)))
+;; Group awesome-tab with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+;; All buffer name start with * will group to \"Emacs\".
+;; Other buffer group by `awesome-tab-get-group-name' with project name."
+;;     (list
+;;      (cond
+;;       ((or (string-equal "*" (substring (buffer-name) 0 1))
+;;            (memq major-mode '(magit-process-mode
+;;                               magit-status-mode
+;;                               magit-diff-mode
+;;                               magit-log-mode
+;;                               magit-file-mode
+;;                               magit-blob-mode
+  ;;                             magit-blame-mode
+  ;;                             )))
+  ;;      "Emacs")
+  ;;     ((derived-mode-p 'eshell-mode)
+  ;;      "EShell")
+  ;;     ((derived-mode-p 'emacs-lisp-mode)
+  ;;      "Elisp")
+  ;;     ((derived-mode-p 'dired-mode)
+  ;;      "Dired")
+  ;;     ((memq major-mode '(org-mode org-agenda-mode diary-mode))
+  ;;      "OrgMode")
+  ;;     (t
+  ;;      (awesome-tab-get-group-name (current-buffer))))))
+
+  ;; (defun awesome-tab-hide-tab (x)
+  ;;   (let ((name (format "%s" x)))
+  ;;     (or
+  ;;      (string-prefix-p "*epc" name)
+  ;;      (string-prefix-p "*helm" name)
+  ;;      (string-prefix-p "*Compile-Log*" name)
+  ;;      (string-prefix-p "*lsp" name)
+  ;;      (string-prefix-p "*Treemacs-" name)
+  ;;      (string-prefix-p "*shell*" name)
+  ;;      (string-prefix-p "*shell" name)
+  ;;      (string-prefix-p "*dashboard*" name)
+  ;;      (string-prefix-p "*info*" name)
+  ;;      (string-prefix-p "*scratch*" name)
+  ;;      (string-prefix-p "*Messages*" name)
+  ;;      (and (string-prefix-p "magit" name)
+  ;;           (not (file-name-extension name)))
+  ;;      )))
+
+  ;; ;; (setq awesome-tab-label-fixed-length 14)
+
+  ;; (when (display-graphic-p)
+  ;;   (setq awesome-tab-display-icon nil)))
 
 ;;--------------------------------------------------------------------
 ;; treemacs
