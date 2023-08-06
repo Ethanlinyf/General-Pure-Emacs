78d1856307f0de9b906941d4edcb00f06defd86b
Ethanlinyf <e.yflin@gmail.com>
2023-08-06 21:54:27 +1000

    To prettify the process list

--- lisp/init-g-interface.el
+++ lisp/init-g-interface.el
@@ -64,26 +64,26 @@
   :hook (ibuffer-mode . nerd-icons-ibuffer-mode)
   :config
   ;; Whether display the icons.
-(setq nerd-icons-ibuffer-icon t)
+  (setq nerd-icons-ibuffer-icon t)
 
-;; Whether display the colorful icons.
-;; It respects `nerd-icons-color-icons'.
-(setq nerd-icons-ibuffer-color-icon t)
+  ;; Whether display the colorful icons.
+  ;; It respects `nerd-icons-color-icons'.
+  (setq nerd-icons-ibuffer-color-icon t)
 
-;; The default icon size in ibuffer.
-(setq nerd-icons-ibuffer-icon-size 1.0)
+  ;; The default icon size in ibuffer.
+  (setq nerd-icons-ibuffer-icon-size 1.0)
 
-;; Use human readable file size in ibuffer.
-(setq  nerd-icons-ibuffer-human-readable-size t)
+  ;; Use human readable file size in ibuffer.
+  (setq  nerd-icons-ibuffer-human-readable-size t)
 
-;; A list of ways to display buffer lines with `nerd-icons'.
-;; See `ibuffer-formats' for details.
-nerd-icons-ibuffer-formats
+  ;; A list of ways to display buffer lines with `nerd-icons'.
+  ;; See `ibuffer-formats' for details.
+  nerd-icons-ibuffer-formats
 
-;; Slow Rendering
-;; If you experience a slow down in performance when rendering multiple icons simultaneously,
-;; you can try setting the following variable
-(setq inhibit-compacting-font-caches t)
+  ;; Slow Rendering
+  ;; If you experience a slow down in performance when rendering multiple icons simultaneously,
+  ;; you can try setting the following variable
+  (setq inhibit-compacting-font-caches t)
   )
 
 
@@ -143,7 +143,43 @@ nerd-icons-ibuffer-formats
   ;;            ("C-M-<right>"     . frame-right-half)
   ;;            ("C-M-<up>"        . frame-top-half)
   ;;            ("C-M-<down>"      . frame-bottom-half)))
-)
+  )
+
+;; Prettify the process list
+(with-no-warnings
+  (add-hook 'process-menu-mode-hook
+            (lambda ()
+              (setq tabulated-list-format
+                    (vconcat `(("" ,(if (icon-displayable-p) 2 0)))
+                             tabulated-list-format))))
+
+  (defun my-list-processes--prettify ()
+    "Prettify process list."
+    (when-let ((entries tabulated-list-entries))
+      (setq tabulated-list-entries nil)
+      (dolist (p (process-list))
+        (when-let* ((val (cadr (assoc p entries)))
+                    (icon (if (icons-displayable-p)
+                              (concat
+                               " "
+                               (nerd-icons-faicon "nf-fa-bolt" :face 'nerd-icons-lblue))
+                            " x"))
+                    (name (aref val 0))
+                    (pid (aref val 1))
+                    (status (aref val 2))
+                    (status (list status
+                                  'face
+                                  (if (memq status '(stop exit closed failed))
+                                      'error
+                                    'success)))
+                    (buf-label (aref val 3))
+                    (tty (list (aref val 4) 'face 'font-lock-doc-face))
+                    (thread (list (aref val 5) 'face 'font-lock-doc-face))
+                    (cmd (list (aref val 6) 'face 'completions-annotations)))
+          (push (list p (vector icon name pid status buf-label tty thread cmd))
+		        tabulated-list-entries)))))
+  (advice-add #'list-processes--refresh :after #'my-list-processes--prettify))
+
 ;;-------------------------------------------------------------------------------------------------
 (provide 'init-g-interface)
 ;;; init-g-interface.el ends here
