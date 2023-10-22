63b3c2193e9122697cea171da111bce348b2bb65
Ethanlinyf <e.yflin@gmail.com>
2023-10-22 20:53:42 +1000

    emhancements for programming

--- core/init-enhancement.el
+++ core/init-enhancement.el
@@ -4,10 +4,120 @@
 ;; Under ThingsEngine Project: https://www.thethingsengine.org
 
 ;;; Commentary:
-;;
+;; Enhancements for programming
 
 ;;; Code:
+;; Configs for programming languages
+(add-hook 'prog-mode-hook (lambda () (setq-local column-number-mode t)))
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(add-hook 'prog-mode-hook 'electric-pair-mode)
+(add-hook 'prog-mode-hook 'flymake-mode)
+(add-hook 'prog-mode-hook 'hs-minor-mode)
+(add-hook 'prog-mode-hook 'prettify-symbols-mode) ; lambda --> λ
+;; (add-hook 'prog-mode-hook 'which-function-mode)
 
+;; Programming format
+(use-package format-all
+  :ensure t
+  :defer t
+  :hook (prog-mode . format-all-mode)
+  :bind ("C-c f" . #'format-all-region-or-buffer))
+
+;; flymake
+(use-package flymake
+  :hook (prog-mode . flymake-mode)
+  :bind (("M-n" . #'flymake-goto-next-error)
+	 ("M-p" . #'flymake-goto-prev-error)))
+
+;; flyspell
+(use-package flyspell
+  :unless *is-win*
+  :hook (prog-mode . flyspell-prog-mode))
+
+;; ;; iedit - edit same text in one buffer or region
+;; (use-package iedit
+;;   :ensure t
+;;   :bind ("C-;" . iedit-mode))
+
+;; ;; Language Server (eglot - builtin since v29)
+;; (use-package eglot
+;;   :hook (prog-mode . eglot-ensure)
+;;   :bind ("C-c e f" . eglot-format)
+;;   :config
+;;   (advice-add 'eglot-code-action-organize-imports :before #'eglot-format))
+
+(require 'treesit)
+;; M-x `treesit-install-language-grammar` to install language grammar.
+(setq treesit-language-source-alist
+      '((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
+        (c . ("https://github.com/tree-sitter/tree-sitter-c"))
+        (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp"))
+        (css . ("https://github.com/tree-sitter/tree-sitter-css"))
+        (cmake . ("https://github.com/uyha/tree-sitter-cmake"))
+        (csharp     . ("https://github.com/tree-sitter/tree-sitter-c-sharp.git"))
+        (dockerfile . ("https://github.com/camdencheek/tree-sitter-dockerfile"))
+        (elisp . ("https://github.com/Wilfred/tree-sitter-elisp"))
+        (go . ("https://github.com/tree-sitter/tree-sitter-go"))
+        (gomod      . ("https://github.com/camdencheek/tree-sitter-go-mod.git"))
+        (html . ("https://github.com/tree-sitter/tree-sitter-html"))
+        (java       . ("https://github.com/tree-sitter/tree-sitter-java.git"))
+        (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
+        (json . ("https://github.com/tree-sitter/tree-sitter-json"))
+        (lua . ("https://github.com/Azganoth/tree-sitter-lua"))
+        (make . ("https://github.com/alemuller/tree-sitter-make"))
+        (markdown . ("https://github.com/MDeiml/tree-sitter-markdown" nil "tree-sitter-markdown/src"))
+        (ocaml . ("https://github.com/tree-sitter/tree-sitter-ocaml" nil "ocaml/src"))
+        (org . ("https://github.com/milisims/tree-sitter-org"))
+        (python . ("https://github.com/tree-sitter/tree-sitter-python"))
+        (php . ("https://github.com/tree-sitter/tree-sitter-php"))
+        (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "typescript/src"))
+        (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "tsx/src"))
+        (ruby . ("https://github.com/tree-sitter/tree-sitter-ruby"))
+        (rust . ("https://github.com/tree-sitter/tree-sitter-rust"))
+        (sql . ("https://github.com/m-novikov/tree-sitter-sql"))
+        (vue . ("https://github.com/merico-dev/tree-sitter-vue"))
+        (yaml . ("https://github.com/ikatyang/tree-sitter-yaml"))
+        (toml . ("https://github.com/tree-sitter/tree-sitter-toml"))
+        (zig . ("https://github.com/GrayJack/tree-sitter-zig"))))
+
+(setq major-mode-remap-alist
+      '((c-mode          . c-ts-mode)
+        (c++-mode        . c++-ts-mode)
+        (cmake-mode      . cmake-ts-mode)
+        (conf-toml-mode  . toml-ts-mode)
+        (css-mode        . css-ts-mode)
+        (js-mode         . js-ts-mode)
+        (js-json-mode    . json-ts-mode)
+        (python-mode     . python-ts-mode)
+        (sh-mode         . bash-ts-mode)
+        (typescript-mode . typescript-ts-mode)
+        (rust-mode       . rust-ts-mode)
+        (java-mode       . java-ts-mode)
+        ))
+
+(add-hook 'markdown-mode-hook #'(lambda () (treesit-parser-create 'markdown)))
+
+(add-hook 'zig-mode-hook #'(lambda () (treesit-parser-create 'zig)))
+
+(add-hook 'web-mode-hook #'(lambda ()
+                             (let ((file-name (buffer-file-name)))
+                               (when file-name
+                                 (treesit-parser-create
+                                  (pcase (file-name-extension file-name)
+                                    ("vue" 'vue)
+                                    ("html" 'html)
+                                    ("php" 'php))))
+                               )))
+
+(add-hook 'emacs-lisp-mode-hook #'(lambda () (treesit-parser-create 'elisp)))
+(add-hook 'ielm-mode-hook #'(lambda () (treesit-parser-create 'elisp)))
+(add-hook 'json-mode-hook #'(lambda () (treesit-parser-create 'json)))
+(add-hook 'go-mode-hook #'(lambda () (treesit-parser-create 'go)))
+(add-hook 'java-mode-hook #'(lambda () (treesit-parser-create 'java)))
+(add-hook 'java-ts-mode-hook #'(lambda () (treesit-parser-create 'java)))
+(add-hook 'php-mode-hook #'(lambda () (treesit-parser-create 'php)))
+(add-hook 'php-ts-mode-hook #'(lambda () (treesit-parser-create 'php)))
+(add-hook 'java-ts-mode-hook #'(lambda () (treesit-parser-create 'java)))
 
 ;;-------------------------------------------------------------------------------------------------
 (provide 'init-enhancement)
