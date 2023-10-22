ffbbb85c70b4a78246f687e83faf17de12a196af
Ethanlinyf <e.yflin@gmail.com>
2023-10-22 22:03:20 +1000

    add org-roam for mpc-org

--- module/practice/mpc-org.el
+++ module/practice/mpc-org.el
@@ -8,7 +8,14 @@
 
 ;;; Code:
 
-
+(use-package org-roam
+  :ensure t
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert))
+  :config
+  (setq org-roam-database-connector 'sqlite-builtin)
+  (org-roam-db-autosync-mode))
 ;;-------------------------------------------------------------------------------------------------
 (provide 'mpc-org)
 
