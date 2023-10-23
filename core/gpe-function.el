761c7f873395cc64e468b4a7293ed297dcb9a5e1
Ethanlinyf <e.yflin@gmail.com>
2023-10-23 18:11:27 +1000

    add functions

--- core/gpe-function.el
+++ core/gpe-function.el
@@ -8,6 +8,166 @@
 
 ;;; Code:
 
+(defun hidden-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (unless buffer-display-table
+    (setq buffer-display-table (make-display-table)))
+  (aset buffer-display-table ?\^M []))
+
+(defun remove-dos-eol ()
+  "Replace DOS eolns CR LF with Unix eolns CR."
+  (interactive)
+  (goto-char (point-min))
+  (while (search-forward "\r" nil t) (replace-match "")))
+
+;;--------------------------------------------------------------------
+;; https://www.emacswiki.org/emacs/OpenNextLine
+;; Behave like vi's o command
+(defun open-next-line (arg)
+  "Move to the next line and then opens a line.
+    See also `newline-and-indent'."
+  (interactive "p")
+  (end-of-line)
+  (open-line arg)
+  (forward-line 1)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+
+(global-set-key (kbd "C-o") 'open-next-line)
+
+;; Behave like vi's O command
+(defun open-previous-line (arg)
+  "Open a new line before the current one.
+     See also `newline-and-indent'."
+  (interactive "p")
+  (beginning-of-line)
+  (open-line arg)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+
+(global-set-key (kbd "M-o") 'open-previous-line)
+
+;; Autoindent open-*-lines
+(defvar newline-and-indent t
+  "Modify the behavior of the open-*-line functions to cause them to autoindent.")
+
+;;--------------------------------------------------------------------
+;; duplicate a line:
+(defun duplicate-line-or-region-above (&optional reverse)
+  "Duplicate current line or region above.
+By default, duplicate current line above.
+If mark is activate, duplicate region lines above.
+Default duplicate above, unless option REVERSE is non-nil."
+  (interactive)
+  (let ((original-column (current-column))
+        duplicate-content)
+    (if mark-active
+        ;; If mark active.
+        (let ((region-start-pos (region-beginning))
+              (region-end-pos (region-end)))
+          ;; Set duplicate start line position.
+          (setq region-start-pos (progn
+                                   (goto-char region-start-pos)
+                                   (line-beginning-position)))
+          ;; Set duplicate end line position.
+          (setq region-end-pos (progn
+                                 (goto-char region-end-pos)
+                                 (line-end-position)))
+          ;; Get duplicate content.
+          (setq duplicate-content (buffer-substring region-start-pos region-end-pos))
+          (if reverse
+              ;; Go to next line after duplicate end position.
+              (progn
+                (goto-char region-end-pos)
+                (forward-line +1))
+            ;; Otherwise go to duplicate start position.
+            (goto-char region-start-pos)))
+      ;; Otherwise set duplicate content equal current line.
+      (setq duplicate-content (buffer-substring
+                               (line-beginning-position)
+                               (line-end-position)))
+      ;; Just move next line when `reverse' is non-nil.
+      (and reverse (forward-line 1))
+      ;; Move to beginning of line.
+      (beginning-of-line))
+    ;; Open one line.
+    (open-line 1)
+    ;; Insert duplicate content and revert column.
+    (insert duplicate-content)
+    (move-to-column original-column t)))
+
+(defun duplicate-line-or-region-below ()
+  "Duplicate current line or region below.
+By default, duplicate current line below.
+If mark is activate, duplicate region lines below."
+  (interactive)
+  (duplicate-line-or-region-above t))
+
+(defun duplicate-line-above-comment (&optional reverse)
+  "Duplicate current line above, and comment current line."
+  (interactive)
+  (if reverse
+      (duplicate-line-or-region-below)
+    (duplicate-line-or-region-above))
+  (save-excursion
+    (if reverse
+        (forward-line -1)
+      (forward-line +1))
+    (comment-or-uncomment-region+)))
+
+(defun duplicate-line-below-comment ()
+  "Duplicate current line below, and comment current line."
+  (interactive)
+  (duplicate-line-above-comment t))
+
+(defun comment-or-uncomment-region+ ()
+  "This function is to comment or uncomment a line or a region."
+  (interactive)
+  (let (beg end)
+    (if mark-active
+        (progn
+          (setq beg (region-beginning))
+          (setq end (region-end)))
+      (setq beg (line-beginning-position))
+      (setq end (line-end-position)))
+    (save-excursion
+      (comment-or-uncomment-region beg end))))
+;;--------------------------------------------------------------------
+(defun kill-new-line ()
+  "Push current line into the kill ring."
+  (interactive)
+  (kill-new (thing-at-point 'line)))
+(defun icon-displayable-p ()
+  "Return non-nil if the icons are displayable."
+  (and (featurep 'nerd-icons)
+       (require 'nerd-icons nil t)))
+;;--------------------------------------------------------------------
+(defun remember-init ()
+  "Remember current position and setup."
+  (interactive)
+  (point-to-register 8)
+  (message "Have remember one position"))
+
+(defun remember-jump ()
+  "Jump to latest position and setup."
+  (interactive)
+  (let ((tmp (point-marker)))
+    (jump-to-register 8)
+    (set-register 8 tmp))
+  (message "Have back to remember position"))
+
+;;--------------------------------------------------------------------
+(defun gpe-unmark-all-buffers ()
+  "Unmark all have marked buffers."
+  (interactive)
+  (let ((current-element (current-buffer)))
+    (save-excursion
+      (dolist (element (buffer-list))
+        (set-buffer element)
+        (deactivate-mark)))
+    (switch-to-buffer current-element)
+    (deactivate-mark)))
 
 ;;-------------------------------------------------------------------------------------------------
 (provide 'gpe-function)
