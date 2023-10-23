9aeb444875de7da465afa824d9e4c3daf34f5a24
Ethanlinyf <e.yflin@gmail.com>
2023-10-23 17:25:33 +1000

    add hippie implementation

--- core/init-s-builtin.el
+++ core/init-s-builtin.el
@@ -74,6 +74,20 @@
 (require 'which-key)
 (add-hook 'after-init-hook #'which-key-mode)
 
+;; enable hippie implementation
+(setq hippie-expand-try-function-list '(try-expand-debbrev
+					try-expand-debbrev-all-buffers
+					try-expand-debbrev-from-kill
+					try-complete-file-name-partially
+					try-complete-file-name
+					try-expand-all-abbrevs
+					try-expand-list
+					try-expand-line
+					try-complete-lisp-symbol-partially
+					try-complete-lisp-symbol))
+
+(global-set-key (kbd "s-/") 'hippie-expand)
+
 ;;-------------------------------------------------------------------------------------------------
 (provide 'init-s-builtin)
 
