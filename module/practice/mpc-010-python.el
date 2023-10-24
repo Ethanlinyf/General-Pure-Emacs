7615d2cdf51815b17962e78b7aef7572da8bb1e1
Ethanlinyf <e.yflin@gmail.com>
2023-10-24 20:31:42 +1000

    enhance python programming

--- module/practice/mpc-010-python.el
+++ module/practice/mpc-010-python.el
@@ -7,7 +7,23 @@
 ;;
 
 ;;; Code:
+(use-package python
+  :ensure nil
+  :hook (inferior-python-mode . (lambda ()
+                                  (process-query-on-exit-flag
+                                   (get-process "Python"))))
+  :init
+  (setq python-shell-completion-native-enable t)
+  :config
+  ;; Default to python3, differ from python2.
+  (setq python-indent-offset 4)
+  (when (and (executable-find "python3")
+             (string= python-shell-interpreter "python3"))
+    (setq python-shell-interpreter "python3")))
 
+;; Live Coding in Python
+(use-package live-py-mode
+  :ensure t)
 
 ;;-------------------------------------------------------------------------------------------------
 (provide 'mpc-010-python)
