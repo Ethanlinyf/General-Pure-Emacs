3db769fd1a88ca5f2b523a26e53f7418b17e17a4
Ethanlinyf <e.yflin@gmail.com>
2023-10-24 17:28:44 +1000

    add tex and markdown

--- module/practice/mpc-001-markdown.el
+++ module/practice/mpc-001-markdown.el
@@ -9,7 +9,112 @@
 ;;; Code:
 
 (use-package markdown-mode
-  :ensure t)
+  :mode (("README\\.md\\'" . gfm-mode))
+  :init
+  (setq markdown-enable-wiki-links t
+        markdown-italic-underscore t
+        markdown-asymmetric-header t
+        markdown-make-gfm-checkboxes-buttons t
+        markdown-gfm-uppercase-checkbox t
+        markdown-fontify-code-blocks-natively t
+
+        markdown-content-type "application/xhtml+xml"
+        markdown-css-paths '("https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.min.css"
+                             "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/github.min.css")
+        markdown-xhtml-header-content "
+<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+<style>
+body {
+  box-sizing: border-box;
+  max-width: 740px;
+  width: 100%;
+  margin: 40px auto;
+  padding: 0 10px;
+}
+</style>
+
+<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/default.min.css'>
+<script src='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/highlight.min.js'></script>
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.classList.add('markdown-body');
+  document.querySelectorAll('pre code').forEach((code) => {
+    if (code.className != 'mermaid') {
+      hljs.highlightBlock(code);
+    }
+  });
+});
+</script>
+
+<script src='https://unpkg.com/mermaid@8.4.8/dist/mermaid.min.js'></script>
+<script>
+mermaid.initialize({
+  theme: 'default',  // default, forest, dark, neutral
+  startOnLoad: true
+});
+</script>
+"
+        markdown-gfm-additional-languages "Mermaid")
+
+  ;; `multimarkdown' is necessary for `highlight.js' and `mermaid.js'
+  (when (executable-find "multimarkdown")
+    (setq markdown-command "multimarkdown"))
+  :config
+  ;; Support `mermaid'
+  (add-to-list 'markdown-code-lang-modes '("mermaid" . mermaid-mode))
+
+  (with-no-warnings
+    ;; Use `which-key' instead
+    (advice-add #'markdown--command-map-prompt :override #'ignore)
+    (advice-add #'markdown--style-map-prompt   :override #'ignore)
+
+    ;; Preview with built-in webkit
+    (defun my-markdown-export-and-preview (fn)
+      "Preview with `xwidget' if applicable, otherwise with the default browser."
+      (if (featurep 'xwidget-internal)
+          (centaur-webkit-browse-url (concat "file://" (markdown-export)) t)
+        (funcall fn)))
+    (advice-add #'markdown-export-and-preview :around #'my-markdown-export-and-preview))
+
+  ;; Preview via `grip'
+  ;; Install: pip install grip
+  (use-package grip-mode
+    :defines org-mode-map
+    :bind (:map markdown-mode-command-map
+                ("g" . grip-mode))
+    :init
+    (with-eval-after-load 'org
+      (bind-key "C-c C-g" #'grip-mode org-mode-map))
+
+    (setq grip-update-after-change nil)
+    (when-let ((credential (auth-source-user-and-password "api.github.com")))
+      (setq grip-github-user (car credential)
+            grip-github-password (cadr credential))))
+
+  ;; Table of contents
+  (use-package markdown-toc
+    :diminish
+    :bind (:map markdown-mode-command-map
+                ("r" . markdown-toc-generate-or-refresh-toc))
+    :hook (markdown-mode . markdown-toc-mode)
+    :init (setq markdown-toc-indentation-space 2
+                markdown-toc-header-toc-title "\n## Table of Contents"
+                markdown-toc-user-toc-structure-manipulation-fn 'cdr)
+    :config
+    (with-no-warnings
+      (define-advice markdown-toc-generate-toc (:around (fn &rest args) lsp)
+        "Generate or refresh toc after disabling lsp."
+        (cond
+         ((bound-and-true-p lsp-managed-mode)
+          (lsp-managed-mode -1)
+          (apply fn args)
+          (lsp-managed-mode 1))
+         ((bound-and-true-p eglot--manage-mode)
+          (eglot--manage-mode -1)
+          (apply fn args)
+          (eglot--manage-mode 1))
+         (t
+          (apply fn args)))))))
 ;;-------------------------------------------------------------------------------------------------
 (provide 'mpc-001-markdown)
 
--- module/practice/mpc-011-tex.el
+++ module/practice/mpc-011-tex.el
@@ -8,6 +8,114 @@
 
 ;;; Code:
 
+(add-hook 'LaTeX-mode-hook 'lsp-bridge-mode)
+
+(use-package auctex
+  :ensure t
+  :init
+  (load "auctex.el" nil t t)
+  :config
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+  (setq-default TeX-master nil))
+
+;;--------------------------------------------------------------------
+;; REFTEX Settings
+(require 'reftex)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+(setq reftex-enable-partial-scans t)
+(setq reftex-save-parse-info t)
+(setq reftex-use-multiple-selection-buffers t)
+(setq reftex-toc-split-windows-horizontally t)                       ; *toc*buffer on left。
+(setq reftex-toc-split-windows-fraction 0.2)                         ; *toc*buffer ratio。
+(autoload 'reftex-mode "reftex" "RefTeX Minor Mode" t)
+(autoload 'turn-on-reftex "reftex" "RefTeX Minor Mode" nil)
+(autoload 'reftex-citation "reftex-cite" "Make citation" nil)
+(autoload 'reftex-index-phrase-mode "reftex-index" "Phrase mode" t)
+
+;;--------------------------------------------------------------------
+;; CDLATEX Settings
+(use-package cdlatex
+  :ensure t
+  :hook
+  (LaTeX-mode . turn-on-cdlatex))
+
+;;--------------------------------------------------------------------
+;; pdf-tools
+(use-package pdf-tools
+  :ensure t
+  :custom
+  (pdf-view-display-size 'fit-width)
+  (pdf-annot-activate-created-annotations t "automatically annotate highlights")
+  :config
+  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward)
+  :hook
+  (pdf-view-mode . (lambda() (setq display-line-numbers-mode nil))))
+
+;;--------------------------------------------------------------------
+(add-hook 'LaTeX-mode-hook (lambda ()
+                             (pdf-tools-install)
+                             (require 'tex-site)
+                             (setq pdf-view-use-scaling t)
+                             (TeX-fold-mode 1)
+                             (auto-fill-mode 1)
+
+                             (flyspell-mode 1)
+                             (setq flyspell-sort-corrections nil)
+                             (setq flyspell-doublon-as-error-flag nil)
+
+                             (setq split-width-threshold 80) ;  pdf-tool to open a pdf in the right side
+                             (turn-on-auto-fill)             ; LaTeX mode，turn off auto fold
+                             (latex-math-mode 1)
+                             (outline-minor-mode 1)
+                             (imenu-add-menubar-index)
+
+                             (setq TeX-show-compilation nil) ; NOT display compilation windows
+                             (setq TeX-global-PDF-mode t)    ; PDF mode enable, not plain
+                             ;;(setq TeX-engine 'default)      ; use xelatex default
+                             (setq TeX-clean-confirm nil)
+                             (setq TeX-save-query nil)
+
+                             (setq font-latex-fontify-script t)
+                             (define-key LaTeX-mode-map (kbd "TAB") 'TeX-complete-symbol)
+                             ;;(setq TeX-electric-escape t)      ; press \ then, jump to mini-buffer to input commands
+                             ;;(setq TeX-view-program-list '(("Evince" "evince %o"))) ;;
+                             ;;(setq TeX-view-program-selection '((output-pdf "Evince")))
+                             (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+                                   TeX-view-program-list '(("PDF Tools" TeX-pdf-tools-sync-view))
+                                   TeX-source-correlate-start-server t)
+                             ;;(add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
+                             ;;(setq TeX-command-default "XeLaTeX")
+                             (add-to-list 'TeX-command-list '("LaTeX" "%`pdflatex -shell-escape --synctex=1%(mode)%' %t" TeX-run-TeX nil t))
+                             (setq TeX-command-default "LaTeX")
+                             ;;(setq TeX-command-default "pdflatex --synctex=1")
+
+                             (setq TeX-fold-env-spec-list (quote (("[comment]" ("comment")) ("[figure]" ("figure")) ("[table]" ("table"))("[itemize]"("itemize"))("[enumerate]"("enumerate"))("[description]"("description"))("[overpic]"("overpic"))("[tabularx]"("tabularx"))("[code]"("code"))("[shell]"("shell")))))
+
+
+                             (define-key LaTeX-mode-map (kbd "C-c C-p") 'reftex-parse-all)
+                             (define-key LaTeX-mode-map (kbd "C-c C-g") #'pdf-sync-forward-search)
+
+                             (setq LaTeX-section-hook
+                                   '(LaTeX-section-heading
+                                     LaTeX-section-title
+                                     LaTeX-section-toc
+                                     LaTeX-section-section
+                                     LaTeX-section-label))
+
+                             (setq pdf-sync-backward-display-action t
+                                   pdf-sync-forward-display-action t
+                                   TeX-source-correlate-mode t
+                                   TeX-source-correlate-method '(
+                                                                 (dvi . source-specials)
+                                                                 (pdf . synctex))
+                                   TeX-source-correlate-start-server t  ; [C-c C-g] to switch between source code and PDF
+                                   reftex-plug-into-AUCTeX t)
+                             (add-hook 'TeX-after-compilation-finished-functions
+                                       #'TeX-revert-document-buffer) ;
+                             (add-hook 'pdf-view-mode-hook (lambda() (display-line-numbers-mode -1)))
+                             ))
 
 ;;-------------------------------------------------------------------------------------------------
 (provide 'mpc-011-tex)
