7dc83e5ad32b2dc29838d6388b24df5f110168f6
Ethanlinyf <e.yflin@gmail.com>
2023-10-30 23:35:52 +1000

    shortcut for the brainstrom.org file

--- core/site-lisp/gpe-custom.el
+++ core/site-lisp/gpe-custom.el
@@ -14,6 +14,14 @@
 ;; (add-to-list 'default-frame-alist '(drag-internal-border . 1))
 ;; (add-to-list 'default-frame-alist '(internal-border-width . 5))
 
+;; To open a specific file
+(defun open-org-brainstom-file()
+  "Open a org file for note taken."
+  (interactive)
+  (find-file "~/Documents/Org/bainstorm.org"))
+(global-set-key (kbd "s-4") 'open-org-brainstom-file)
+
+(set-face-attribute 'default nil :height 180);;
 ;;-------------------------------------------------------------------------------------------------
 (provide 'gpe-custom)
 
